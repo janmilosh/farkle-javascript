1a7978a8c861578b0a6058041dc293faa4dcd711
Jan Milosh <janmilosh@gmail.com>
2013-08-26 23:23:27 -0400

    Add more functions

--- javascripts/farkle.js
+++ javascripts/farkle.js
@@ -14,7 +14,9 @@ player1.roundScore = 0;
 player2.roundScore = 0;
 player1.rollScore = 0;
 player2.rollScore = 0;
-var playerDice = [false,false,false,false,false,false];	// scoring array
+var player;
+var roundScore;
+var playerDice = [];	// scoring array
 // ---------------------------------------------------------
 //             toggle instructions on click
 // ---------------------------------------------------------
@@ -39,7 +41,7 @@ function onePlayer() {
 	$(document).ready(function() {	
 		$("#singular-plural").text("name");
 		$(".player2-name").text(player2.name);
-		$("#one-player").fadeOut();
+		$("#one-player").fadeOut("slow");
 	});
 }
 // ---------------------------------------------------------
@@ -47,12 +49,15 @@ function onePlayer() {
 //                   by adding their names
 // ---------------------------------------------------------
 function addNames() {
-		player1.name = prompt("Player one: please enter your name","Player one name");
-		$(".player1-name").text(player1.name);
-		if (player2.name !== "The House") {
-			player2.name = prompt("Player two: please enter your name","Player two name");
-			$(".player2-name").text(player2.name);
-		}
+	player1.name = prompt("Player one: please enter your name","Player one name");
+	$(".player1-name").text(player1.name);
+	if (player2.name !== "The House") {
+		player2.name = prompt("Player two: please enter your name","Player two name");
+		$(".player2-name").text(player2.name);
+	}
+	if (player2.name !== "The House") {
+		$("#one-player").fadeOut("slow");
+	}
 }
 // ---------------------------------------------------------
 //       function for ending game with page reload
@@ -90,9 +95,45 @@ function rollDice(diceArray) {
 	console.log(diceArray.length);
 }
 // ---------------------------------------------------------
+//              function to complete a round
+// ---------------------------------------------------------
+function round(player) {
+	roundScore = 0;
+	playerDice = [false,false,false,false,false,false];
+
+
+
+}
+// ---------------------------------------------------------
 //                       Begin game
 // ---------------------------------------------------------
 $(document).ready(function() {
-	
+  $("#instructions").fadeTo(1000, 0.4).fadeTo(1000, 1) //a couple of fades to draw
+  	.fadeTo(1000, 0.4).fadeTo(1000, 1);		         //attention to the instructions
+  player1.turn = true;
+  player = player1.name;
+  do {
+  	roundScore = round(player);
+  	if (player1.turn == true) { // switch players and add the score
+			player1.turn = false;
+			player2.turn = true;
+			player = player2.name;
+			player1.score = player1.score + roundScore;
+			$("#player1-total").text(player1.score);
+			$("#player1-round").text(player1.roundScore);
+			if (player2.name !== "The House") {
+				$("#instructions").text(player2.name + ": start your round by rolling the dice.");
+			}
+		} else {
+			player1.turn = true;
+			player2.turn = false;
+			player = player1.name;
+			player2.score = player2.score + roundScore;
+			$("#player2-total").text(player2.score);
+			$("#player2-round").text(player2.roundScore);
+			$("#instructions").text(player1.name + ": start your round by rolling the dice.");
+		}
 
+
+  } while((player1.score < 10000) && (player2.score < 10000));
 });
